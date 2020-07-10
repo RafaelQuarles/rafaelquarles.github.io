668bdd50123fbe0918daabcbab9b015aa69e18be
RafaelQuarles <RafaelQuarles@yahoo.com>
2020-07-10 16:36:47 -0400

    initial commit

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -6,33 +6,32 @@ class Portfolio extends Component {
     if(this.props.data){
       var projects = this.props.data.projects.map(function(projects){
         var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
+        return <div key={projects.title} className="p-column">
+                  <div className="p-card">
+                    <img alt={projects.title} src={projectImage} />
+                    <div className="p-body">
+
+                      <div className="p-data">
+                        <h5>{projects.title}</h5>
+                        <p>{projects.category}</p>
+                      </div>
+                      <a href={projects.url} className="button p-button"><i className="fa fa-link"></i> View Online</a>
+                    </div>
                   </div>
                 </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
-          </div>
-        </div>
       })
     }
 
     return (
       <section id="portfolio">
 
-      <div className="row">
+      <div className="p-row">
 
-         <div className="twelve columns collapsed">
+         <div className="twelve columns">
 
             <h1>Check Out Some of My Works.</h1>
 
-            <div id="portfolio-wrapper" className="bgrid-quarters cf align-center">
+            <div className="align-center">
                 {projects}
             </div>
           </div>
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -4,30 +4,31 @@ class Resume extends Component {
   render() {
 
     if(this.props.data){
-      var skillmessage = this.props.data.skillmessage;
+      //var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
         <p>{education.description}</p></div>
       })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
-        </div>
-      })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
+      // var work = this.props.data.work.map(function(work){
+      //   return <div key={work.company}><h3>{work.company}</h3>
+      //       <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+      //       <p>{work.description}</p>
+      //   </div>
+      // })
+      // var skills = this.props.data.skills.map(function(skills){
+      //   var className = 'bar-expand '+skills.name.toLowerCase();
+      //   return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+      // })
       var programs = this.props.data.programs.map(function(programs){
         var programsImage = 'images/programs/'+programs.image;
-        return <div className="columns programs">
-        <div key={programs.title}>
-        <img alt={programs.image} src={programsImage} />
-        <h5 >{programs.title}</h5>
-        {/*<p>{programs.description}</p>*/}</div>
-        </div>
+        return <li key={programs.title} className="columns programs">
+          <div>
+          <img alt={programs.image} src={programsImage} />
+          <h5 >{programs.title}</h5>
+          {/*<p>{programs.description}</p>*/}
+          </div>
+        </li>
       })
     }
 
