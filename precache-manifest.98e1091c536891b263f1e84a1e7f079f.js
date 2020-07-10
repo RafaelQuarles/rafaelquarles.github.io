e123265d82a8f7bd0132d0cd43ea7f1935bf1bdc
RafaelQuarles <RafaelQuarles@yahoo.com>
2020-07-10 17:10:49 -0400

    Updates

---
+++ precache-manifest.98e1091c536891b263f1e84a1e7f079f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdffb33b0fc5ad7ce117148a0e29c812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "500751006cd84997e4c5",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a40a30004ab536fa5c08",
+    "url": "/static/js/2.75bce130.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.75bce130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500751006cd84997e4c5",
+    "url": "/static/js/main.f3dbf79c.chunk.js"
+  },
+  {
+    "revision": "6a99b4de247390dc714c",
+    "url": "/static/js/runtime-main.e32b5e9b.js"
+  }
+]);
